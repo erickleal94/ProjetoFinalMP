989d8a39e62a07a22f2bd67b4247629d60ef6dd3
Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>
2016-06-12 08:56:18 -0300

    Cria header privado
    Cria header privado que compartilha as estruturas usadas
    Todo código que deseje usar Celula_priv ou grafo_priv
    deve incluí-lo.
    
    Signed-off-by: Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>

--- Fase1/include/grafo.h
+++ Fase1/include/grafo.h
@@ -7,38 +7,38 @@ typedef enum resp {
 } resposta;
 
 
-struct grafo;
-typedef grafo grafo_t;
-struct Celula;
-typedef Celula Celula_t;
+struct grafo_priv;
+typedef struct grafo_priv grafo_priv_t;
+struct Celula_priv;
+typedef struct Celula_priv Celula_priv_t;
 
-grafo_t *criar_grafo(void);
-grafo_t *deletar_grafo(grafo_t *meu_grafo);
+grafo_priv_t *criar_grafo(void);
+grafo_priv_t *deletar_grafo(grafo_priv_t *meu_grafo);
 
-resposta existe_vert(const grafo_t *meu_grafo, int id_externo);
-resposta existe_aresta(const grafo_t *meu_grafo, int id_externo1, int id_externo2);
+resposta existe_vert(const grafo_priv_t *meu_grafo, int id_externo);
+resposta existe_aresta(const grafo_priv_t *meu_grafo, int id_externo1, int id_externo2);
 
-int achar_id(const grafo_t *meu_grafo, int id_externo);
-char *achar_nome(const grafo_t *meu_grafo, int id_externo);
+int achar_id(const grafo_priv_t *meu_grafo, int id_externo);
+char *achar_nome(const grafo_priv_t *meu_grafo, int id_externo);
 
-void inserir_vert(grafo_t *meu_grafo, Celula_t *celula);
-void inserir_aresta(grafo_t *meu_grafo, Celula_t *celul1, Celula_t *celula2, const double peso);
+void inserir_vert(grafo_priv_t *meu_grafo, Celula_priv_t *celula);
+void inserir_aresta(grafo_priv_t *meu_grafo, int id1, Celula_priv_t *celula2, const double peso);
 
-void Ler_Tarefas(Celula_t *celula, const char *NomeArq);
+void Ler_Tarefas(Celula_priv_t *celula, const char *NomeArq);
 
-void remover_vert(grafo_t *meu_grafo, Celula_t *celula);
-void remover_aresta(grafo_t *meu_grafo, Celula_t *celula1, Celula_t *celula2);
+void remover_vert(grafo_priv_t *meu_grafo, Celula_priv_t *celula);
+void remover_aresta(grafo_priv_t *meu_grafo, Celula_priv_t *celula1, Celula_priv_t *celula2);
 
-void Imprime_Tarefas(Celula_t *celula);
-void Grava_Arq(Celula_t *celula, char *NomeArq);
+void Imprime_Tarefas(Celula_priv_t *celula);
+void Grava_Arq(Celula_priv_t *celula, char *NomeArq);
 
-int maior_id(const grafo_t *meu_grafo);
+int maior_id(const grafo_priv_t *meu_grafo);
 
-int num_vert(const grafo_t *meu_grafo);
-int num_arestas(const grafo_t *meu_grafo);
+int num_vert(const grafo_priv_t *meu_grafo);
+int num_arestas(const grafo_priv_t *meu_grafo);
 
-double menor_caminho(const grafo_t *meu_grafo, const char *inicio, const char *fim);
+double menor_caminho(const grafo_priv_t *meu_grafo, const char *inicio, const char *fim);
 
-resposta eh_conexo(const grafo_t *meu_grafo);
+resposta eh_conexo(const grafo_priv_t *meu_grafo);
 
 #endif
--- Fase1/src/grafo.c
+++ Fase1/src/grafo.c
@@ -1,66 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <grafo_priv.h>
 #include <grafo.h>
 
 //se for 1 serão escritas mensagens de erro para
 //a saída padrão
 #define DEBUG 0
 
-struct lista_vert;
-
-typedef struct lista_vert_codigo {
-	Celula_t dado; /*nome do vértice, dada pelo usuário*/
-	int id; /*representação interna do vértice, gerado pelo código*/
-	struct lista_vert_codigo *next;
-} lista_vert_codigo_t;
-
-typedef struct lista_aresta {
-	struct lista_vert *destino;
-	double peso;
-	struct lista_aresta *next;
-} lista_aresta_t;
-
-typedef struct lista_vert {
-	int id; /*representação interna do vértice, gerado pelo código*/
-	int id_externo;
-	lista_aresta_t *antecessores;
-	lista_aresta_t *sucessores;
-	struct lista_vert *next;
-} lista_vert_t;
-
-typedef struct lista_origem {
-	lista_vert_t *destino;
-	struct lista_origem *next;
-} lista_origem_t;
-
-typedef struct grafo {
-	lista_vert_codigo_t *tabela; /* tabela nome do vértice - código*/
-	lista_vert_t *vert;
-	lista_origem_t *origem;
-} grafo_t;
-
-
-extern struct Celula{
+grafo_priv_t *criar_grafo(void) {
+	grafo_priv_t *meu_grafo = (grafo_priv_t *) malloc(sizeof(grafo_priv_t));
+	Celula_priv_t *celula = (Celula_priv_t *) malloc(sizeof(Celula_priv_t));
 	
-	int ID_tarefa, executada, duracao, ini_min, pre_req;
-	char nome[200];
-		
-	
-}
-
-void inserir_origem(grafo_t *meu_grafo, Celula_t *celula);
-
-grafo_t *criar_grafo(void) {
-	grafo_t *meu_grafo = (grafo_t *) malloc(sizeof(grafo_t));
-	Celula_t *celula = (Celula_t *) malloc(sizeof(Celula_t));
-	
-	celula->ID_tarefa = -1;
+	celula->id_externo = -1;
 	celula->executada = 1;
 	celula->duracao = 0;
 	celula->ini_min = 0;
 	celula->pre_req = 0;
-	celula->nome = "";
+	celula->nome[0] = '\0';
 	
 	meu_grafo->tabela = NULL;
 	meu_grafo->vert = NULL;
@@ -71,7 +28,7 @@ grafo_t *criar_grafo(void) {
 	
 	return meu_grafo;
 }
-grafo_t *deletar_grafo(grafo_t *meu_grafo) {
+grafo_priv_t *deletar_grafo(grafo_priv_t *meu_grafo) {
 	lista_vert_codigo_t *iterator;
 	
 	for (iterator = meu_grafo->tabela; iterator != NULL; iterator = meu_grafo->tabela) {
@@ -82,7 +39,7 @@ grafo_t *deletar_grafo(grafo_t *meu_grafo) {
 	return NULL;
 }
 
-resposta existe_vert(const grafo_t *meu_grafo, int id_externo) {
+resposta existe_vert(const grafo_priv_t *meu_grafo, int id_externo) {
 	lista_vert_codigo_t *iterator;
 	
 	for (iterator = meu_grafo->tabela; iterator != NULL; iterator = iterator->next) {
@@ -92,7 +49,7 @@ resposta existe_vert(const grafo_t *meu_grafo, int id_externo) {
 	return FALSE;
 }
 
-resposta existe_aresta(const grafo_t *meu_grafo, int id_externo1, int id_externo2) {
+resposta existe_aresta(const grafo_priv_t *meu_grafo, int id_externo1, int id_externo2) {
 	lista_vert_t *vert_inicial;
 	lista_aresta_t *iterator;
 	
@@ -112,7 +69,7 @@ resposta existe_aresta(const grafo_t *meu_grafo, int id_externo1, int id_externo
 	return FALSE;
 }
 
-int achar_id(const grafo_t *meu_grafo, int id_externo) {
+int achar_id(const grafo_priv_t *meu_grafo, int id_externo) {
 	lista_vert_codigo_t *iterator;
 	
 	for (iterator = meu_grafo->tabela; iterator != NULL; iterator = iterator->next) {
@@ -122,7 +79,7 @@ int achar_id(const grafo_t *meu_grafo, int id_externo) {
 	}
 	return -1;
 }
-char *achar_nome(const grafo_t *meu_grafo, int id_externo) {
+char *achar_nome(const grafo_priv_t *meu_grafo, int id_externo) {
 	lista_vert_codigo_t *iterator;
 	
 	for (iterator = meu_grafo->tabela; iterator != NULL; iterator = iterator->next) {
